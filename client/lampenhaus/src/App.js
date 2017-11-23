e5c4b68ed6b865a25c8fbea49b8462995c9a6a7c
Moritz Hartmann <moritz.hartmann@student.hpi.uni-potsdam.de>
2017-11-23 17:47:57 +0100

    Test react-toolbox

--- client/lampenhaus/src/App.js
+++ client/lampenhaus/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import Button from 'react-toolbox/lib/button/Button';
+
 
 class App extends Component {
   render() {
@@ -10,6 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Button label="Hello World!" />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
