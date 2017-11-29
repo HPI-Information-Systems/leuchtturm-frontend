576cd9c43b288274851e9915ad8c656067f98dd4
Jakob Edding <jakob.edding@student.hpi.uni-potsdam.de>
2017-11-29 13:43:21 +0100

    Add a basic searchbar meta#112
    - Use InputGroup from reactstrap for searchbar
    - Add callbacks for changing search term and submit

--- client/lampenhaus/src/components/App/App.js
+++ client/lampenhaus/src/components/App/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import { InputGroup, InputGroupButton, Input } from 'reactstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    };
+  }
+
+  handleSearchInputChange(event) {
+    this.setState({inputValue: event.target.value});
+  }
+
+  handleSearchClick() {
+    console.log(this.state.inputValue)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Lampenhaus</h1>
         </header>
+        <InputGroup>
+          <Input placeholder="Ken Lay" defaultValue={this.state.inputValue} onChange={(e) => this.handleSearchInputChange(e)}/>
+          <InputGroupButton color="primary" onClick={() => this.handleSearchClick()}>Search</InputGroupButton>
+        </InputGroup>
       </div>
     );
   }
