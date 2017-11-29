5baf9ef594906db5cd4290d774eab82f1d36cb7d
Jakob Edding <jakob.edding@student.hpi.uni-potsdam.de>
2017-11-29 12:01:05 +0100

    Delete bootstrap React page

--- client/lampenhaus/src/components/App/App.js
+++ client/lampenhaus/src/components/App/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
-import logo from '../../assets/logo.svg';
 import './App.css';
-import ExampleComponent from '../ExampleComponent/ExampleComponent'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Lampenhaus</h1>
         </header>
-        <ExampleComponent/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
