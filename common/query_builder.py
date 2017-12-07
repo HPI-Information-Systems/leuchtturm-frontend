daa6f15bb92f8b95b57119032f2d64530c3e0ce1
Dennis Kipping <dennis.kipping@student.hpi.uni-potsdam.de>
2017-12-07 15:56:22 +0100

    config file for host and port, input of querybuilder now only flag for dev or production

--- common/query_builder.py
+++ common/query_builder.py
@@ -1,20 +1,29 @@
 """This module builds queries and passes them to the interface."""
 
 from requester_interface import RequesterInterface
-
+import configparser
 
 class QueryBuilder():
     """Class for building queries on high level."""
 
     def __init__(self,
-                 host,
-                 port,
+                 flag,
                  core,
                  search_term,
                  response_format='json',
                  limit=10,
                  snippets=False):
-        """Initialize."""
+        """Initialize. Provide flag: 'dev' or 'production'."""
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+        port = str(self.config['CONNECTION']['Port'])
+        if flag == 'dev':
+            host = 'localhost'
+        elif flag == 'production':
+            host = str(self.config['CONNECTION']['Host'])
+        else:
+            raise ValueError('Flag has to be "dev" or "production".')
+            
         self.url = 'http://' + host + ':' + port + '/solr/'
         self.core = core
         self.params = {'qt': 'select'}
@@ -24,6 +33,7 @@ class QueryBuilder():
         self.params['hl'] = snippets
 
         self.requester = RequesterInterface(self.url, self.core, response_format)
+        print(self.url)
 
     def send(self):
         """Send a simple query."""
@@ -47,6 +57,6 @@ class Query():
 """
 Test
 
-qb = QueryBuilder('localhost', '8983', 'entities', 'json')
+qb = QueryBuilder('dev', 'entities', 'json')
 qb.send_standard_query('Gooddell')
 """
