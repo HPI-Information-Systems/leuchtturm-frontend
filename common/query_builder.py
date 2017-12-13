5eb5d84d5f3854a901922f6399a3364a27b8ec45
Moritz Hartmann <moritz.hartmann@student.hpi.uni-potsdam.de>
2017-12-13 12:12:58 +0100

    Extend query builder

--- common/query_builder.py
+++ common/query_builder.py
@@ -1,9 +1,18 @@
 """This module builds queries and passes them to the interface."""
 
-from .requester_interface import RequesterInterface
+from requester_interface import RequesterInterface
 import configparser
 from os import environ as env
 from os import path
+
+DEFAULT_SHOW_FIELDS = '*'
+DEFAULT_SEARCH_FIELD = None
+DEFAULT_LIMIT = 10
+DEFAULT_SNIPPETS = False
+DEFAULT_PAGE = 0
+DEFAULT_RESPONSE_FORMAT = 'json'
+
+LEUCHTTURM = 'LEUCHTTURMMODE'
 
 
 class QueryBuilder():
@@ -12,29 +21,50 @@ class QueryBuilder():
     def __init__(self,
                  core,
                  search_term,
-                 show_fields='*',
-                 limit=10,
-                 snippets=False,
-                 response_format='json'):
+                 search_field=DEFAULT_SEARCH_FIELD,
+                 show_fields=DEFAULT_SHOW_FIELDS,
+                 limit=DEFAULT_LIMIT,
+                 page=DEFAULT_PAGE,
+                 snippets=DEFAULT_SNIPPETS,
+                 response_format=DEFAULT_RESPONSE_FORMAT):
         """Initialize. Provide flag: 'dev' or 'production'."""
+        if search_field is '':
+            search_field = DEFAULT_SEARCH_FIELD
+        if core is None or search_term is None:
+            raise ValueError('core and search_term need a value')
+        if show_fields is None:
+            show_fields = DEFAULT_SHOW_FIELDS
+        if limit is None:
+            limit = DEFAULT_LIMIT
+        if page is None:
+            page = DEFAULT_PAGE
+        if snippets is None:
+            snippets = DEFAULT_SNIPPETS
+        if response_format is None:
+            response_format = DEFAULT_RESPONSE_FORMAT
+
         configpath = path.join(path.dirname(path.abspath(__file__)), 'config.ini')
         self.config = configparser.ConfigParser()
         self.config.read(configpath)
         port = str(self.config['CONNECTION']['Port'])
-        if env['LEUCHTTURMMODE'] == 'DEVELOP':
-            host = 'localhost'
-        elif env['LEUCHTTURMMODE'] == 'PRODUCTION':
-            host = str(self.config['CONNECTION']['Host'])
-        else:
+        if LEUCHTTURM not in env:
             raise ValueError('Environment variable "LEUCHTTURMMODE" has to be set to "DEVELOP" or "PRODUCTION".')
+        elif env[LEUCHTTURM] == 'DEVELOP':
+            host = 'localhost'
+        elif env[LEUCHTTURM] == 'PRODUCTION':
+            host = str(self.config['CONNECTION']['Host'])
 
         self.url = 'http://' + host + ':' + port + '/solr/'
         self.core = core
         self.params = {'qt': 'select'}
-        self.params['q'] = search_term
-        self.params['fl'] = show_fields # comma seperated
+        if search_field is not None:
+            self.params['q'] = str(search_field) + ':*' + search_term + '*'
+        else:
+            self.params['q'] = '*' + str(search_term) + '*'
+        self.params['fl'] = show_fields  # comma seperated
         self.params['wt'] = response_format
         self.params['rows'] = limit
+        self.params['start'] = page
         self.params['hl'] = str(snippets).lower()
 
         self.requester = RequesterInterface(self.url, self.core, response_format)
