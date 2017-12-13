0900aa48db7a24044b11870753a8c2bcdd584dd6
Hendrik Schmidt <hendrik.schmidt@student.hpi.uni-potsdam.de>
2017-12-13 16:43:28 +0100

    exiting comment

--- client/lampenhaus/src/index.js
+++ client/lampenhaus/src/index.js
@@ -12,7 +12,7 @@ import './index.css';
 const store = createStore(
     reducers,
     applyMiddleware(thunkMiddleware),
-    // config for redux chrome dev tool
+    // config for redux chrome dev tool, doesnt work with thunk apparently
     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
