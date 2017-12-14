d3c5eaa6fa9250eaf7035a02cbac42e18af9f5c5
Jakob Edding <jakob.edding@student.hpi.uni-potsdam.de>
2017-12-14 15:56:42 +0100

    Expose flask app to the network meta#88

--- deploy_flask_app.sh
+++ deploy_flask_app.sh
@@ -4,4 +4,5 @@ pip install -r requirements-dev.txt
 export LEUCHTTURMMODE="DEVELOP"
 export FLASK_APP=autoapp.py
 export FLASK_DEBUG=1
-flask run
+# host flag makes sure that flask app is accessible from the network
+flask run --host=0.0.0.0
