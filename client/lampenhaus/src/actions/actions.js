1e1b25fd9bf17f2302749a5ea783073d0b1d7d8e
Jakob Edding <jakob.edding@student.hpi.uni-potsdam.de>
2017-12-14 17:01:25 +0100

    Quickfix flask api host in React files meta#88

--- client/lampenhaus/src/actions/actions.js
+++ client/lampenhaus/src/actions/actions.js
@@ -23,8 +23,8 @@ export const fetchResults = searchTerm => {
     return dispatch => {
 
         dispatch(submitSearch(searchTerm));
-
-        return fetch('http://localhost:5000/api/search?search_term=' + searchTerm + '&offset=4')
+        // TODO: remove hardcoded host
+        return fetch('http://b9448.byod.hpi.de:5000/api/search?search_term=' + searchTerm + '&offset=4')
             .then(
                 response => response.json(),
                 error => console.log('An error occurred.', error)
