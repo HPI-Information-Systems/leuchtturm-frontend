e74644abc9995bd7ebf720b9630607f788b63e3c
Jakob Edding <jakob.edding@student.hpi.uni-potsdam.de>
2017-12-14 16:45:06 +0100

    Make deployed flask app use solr instance running on cluster node b1184 meta#88

--- deploy_flask_app.sh
+++ deploy_flask_app.sh
@@ -8,7 +8,7 @@ cd ../..
 
 # RUN FLASK APP
 pip install -r requirements-dev.txt
-export LEUCHTTURMMODE="DEVELOP"
+export LEUCHTTURMMODE="PRODUCTION"
 export FLASK_APP=autoapp.py
 export FLASK_DEBUG=1
 # host flag makes sure that flask app is accessible from the network
