351a46f1a4ce7607e3f5e4e02f7ba3cf00d6a3c5
Jakob Edding <jakob.edding@student.hpi.uni-potsdam.de>
2017-12-19 17:40:15 +0100

    Add tests for response JSON structure, show_fields, search_field, pagination meta#111

--- tests/test_search_api.py
+++ tests/test_search_api.py
@@ -11,9 +11,61 @@ class TestSearch(MetaTestSearch):
             'search_term': 'and'
         }
         res = client.get(url_for('api.search', **self.params))
+
         assert res.json['responseHeader']['message'] == 'All Good'
+
+        # check structure of returned JSON
+        assert 'response' in res.json
+        assert 'responseHeader' in res.json
+        for key in ['message', 'responseTime', 'status']:
+            assert key in res.json['responseHeader']
+        for key in ['numFound', 'results']:
+            assert key in res.json['response']
+        for key in ['body', 'doc_id', 'entities', 'header', 'lang', 'raw']:
+            assert key in res.json['response']['results'][0]
+        for key in ['Date', 'Subject', 'recipients', 'sender']:
+            assert key in res.json['response']['results'][0]['header']
+        entities = res.json['response']['results'][0]['entities']
+        for key in ['CARDINAL', 'DATE', 'GPE']:
+            assert key in entities
+            assert 'entity' in entities[key][0]
+            assert 'entity_count' in entities[key][0]
+
 
     def test_search_no_search_term(self, client):
         res = client.get(url_for('api.search', **self.params))
         assert res.json['responseHeader']['status'] == 'SyntaxError'
         assert 'stackTrace' in res.json['responseHeader']
+
+    def test_search_show_fields(self, client):
+        self.params = {
+            **self.params,
+            'search_term': 'and',
+            'show_fields': 'body,doc_id',
+        }
+        res = client.get(url_for('api.search', **self.params))
+        assert res.json['responseHeader']['status'] == 'Ok'
+        assert len(res.json['response']['results'][0]) == 2
+
+    def test_search_pagination(self, client):
+        self.params = {
+            **self.params,
+            'search_term': 'and',
+            'offset': 1,
+            'limit': 2
+        }
+        res = client.get(url_for('api.search', **self.params))
+        assert res.json['responseHeader']['status'] == 'Ok'
+        assert len(res.json['response']['results']) == self.params['limit']
+        assert res.json['response']['results'][0]['doc_id'][0] == 2
+        assert res.json['response']['results'][1]['doc_id'][0] == 3
+
+    def test_search_search_field(self, client):
+        self.params = {
+            **self.params,
+            'search_term': 'and'
+        }
+        res_no_search_field = client.get(url_for('api.search', **self.params))
+        self.params['search_field'] = 'header.Subject'
+        res_search_field_set = client.get(url_for('api.search', **self.params))
+        assert len(res_search_field_set.json['response']['results']) < len(res_no_search_field.json['response']['results'])
