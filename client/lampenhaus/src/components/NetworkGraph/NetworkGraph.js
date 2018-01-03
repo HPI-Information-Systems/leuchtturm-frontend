103d8aba0c5f9a96732471910bd9e96c7171a282
Dennis Kipping <dennis.kipping@student.hpi.uni-potsdam.de>
2018-01-03 17:02:14 +0100

    added mocked filter buttons

--- client/lampenhaus/src/components/NetworkGraph/NetworkGraph.js
+++ client/lampenhaus/src/components/NetworkGraph/NetworkGraph.js
@@ -1,16 +1,22 @@
+import { Button, ButtonGroup } from 'reactstrap';
 import React, { Component } from 'react';
 import { Sigma, NeoCypher, NeoGraphItemsProducers, RelativeSize, RandomizeNodePositions,
     // keep SigmaEnableWebGL to enable webgl renderer, no explicit usage in code necessary
     // eslint-disable-next-line
     SigmaEnableWebGL } from 'react-sigma';
+import './NetworkGraph.css';
 
 this.myProducers = new NeoGraphItemsProducers();
 const myProto = Object.getPrototypeOf(this.myProducers);
 const baseNodeFactory = myProto.node.bind(this.myProducers);
 const baseEdgeFactory = myProto.edge.bind(this.myProducers);
 
+const names = {};
+const mails = {};
+
 class MyGraphItemsProducers {
     static node(n) {
+        names[n.id] = n.properties.name;
         const x = baseNodeFactory(n);
         x.label = n.properties.email;
         x.color = '#3f4fdf';
@@ -18,8 +24,11 @@ class MyGraphItemsProducers {
     }
     static edge(e) {
         const x = baseEdgeFactory(e);
+        mails[e.id] = e.properties.mail_list;
         // eslint-disable-next-line
-        x.label = e.properties.mail_list[1];
+        console.log(mails);
+        const [first] = e.properties.mail_list;
+        x.label = first;
         x.color = '#606166';
         return x;
     }
@@ -29,32 +38,40 @@ class MyGraphItemsProducers {
 class NetworkGraph extends Component {
     render() {
         return (
-            <Sigma
-                style={{
-                    maxWidth: 'inherit',
-                    height: '800px',
-                    background: '#282c34',
-                }}
-                settings={{
-                    drawEdges: true,
-                    clone: false,
-                    defaultNodeColor: '#fff',
-                    defaultLabelColor: '#fff',
-                    defaultLabelSize: 14,
-                }}
-                // eslint-disable-next-line
-                onOverNode={e => console.log(`Mouse over node: ${e.data.node.label}`)}
-            >
-                <NeoCypher
-                    producers={MyGraphItemsProducers}
-                    url="http://b3986.byod.hpi.de:7474"
-                    user=""
-                    password=""
-                    query="MATCH p=()-[r:WRITESTO]->() RETURN p"
-                />
-                <RelativeSize initialSize={15} />
-                <RandomizeNodePositions />
-            </Sigma>
+            <div>
+                <ButtonGroup class="filter-buttons">
+                    <Button>25</Button>
+                    <Button>50</Button>
+                    <Button>100</Button>
+                    <Button>all</Button>
+                </ButtonGroup>
+
+                <Sigma
+                    style={{
+                        maxWidth: 'inherit',
+                        height: '800px',
+                        background: '#282c34',
+                    }}
+                    settings={{
+                        drawEdges: true,
+                        clone: false,
+                        defaultLabelColor: '#fff',
+                        defaultLabelSize: 14,
+                    }}
+                    // eslint-disable-next-line
+                    onClickNode={e => console.log(`Mouse over node: ${e.data.node.label}`)}
+                >
+                    <NeoCypher
+                        producers={MyGraphItemsProducers}
+                        url="http://b3986.byod.hpi.de:7474"
+                        user=""
+                        password=""
+                        query="MATCH p=()-[r:WRITESTO]->() RETURN p"
+                    />
+                    <RelativeSize initialSize={15} />
+                    <RandomizeNodePositions />
+                </Sigma>
+            </div>
         );
     }
 }
