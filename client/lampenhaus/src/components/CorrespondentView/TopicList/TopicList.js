c740c21ce40dc9f8b7175606924097e457407591
Nico Scordialo <nico.scordialo@student.hpi.uni-potsdam.de>
2018-02-22 12:06:41 +0100

    Apply styling to bars and topics. Ref meta#182.

--- client/lampenhaus/src/components/CorrespondentView/TopicList/TopicList.js
+++ client/lampenhaus/src/components/CorrespondentView/TopicList/TopicList.js
@@ -1,9 +1,10 @@
 /* eslint-disable */
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
-import { BarChart, ResponsiveContainer, Bar, Legend, Tooltip, YAxis, XAxis, Label  } from 'recharts';
+import { BarChart, ResponsiveContainer, Bar, Legend, Tooltip, YAxis, XAxis  } from 'recharts';
 import { ListGroup, ListGroupItem, Badge } from 'reactstrap';
 import PropTypes from 'prop-types';
+import './TopicList.css';
 import Spinner from '../../Spinner/Spinner';
 
 // eslint-disable-next-line react/prefer-stateless-function
@@ -20,44 +21,41 @@ class TopicList extends Component {
     showTopic(topic) {
         this.setState({ showResults: true });
         this.state.currentTopic = topic
-        console.log(this.state.currentTopic.words)
-        console.log(this.state.currentTopic.words.split(", "))
     }
 
     render() {
         var label_topics = this.props.topics.map((topic) => {
             return {
             confidence: topic.confidence,
-            words: topic.words.map((word) => {return word.word}).join(", ")
+            words: topic.words.map((word) => {return word.word}).join(" ")
         }})
           
 
         return (
             <div>
-                <ResponsiveContainer width={"100%"} height={250}>
+                <ResponsiveContainer width={"100%"} height={200}>
                     <BarChart data={
                         label_topics
                     }>
-                        <XAxis dataKey="words"  tick={false}>
-                            <Label value="any" />
-                        </XAxis >
+                        <XAxis dataKey="words"  tick={false}/>
                         <YAxis/>
                         <Tooltip />
-                        <Legend />
-                        <Bar dataKey="confidence" fill="#8884d8" onClick={(topic, index) => this.showTopic(topic)} />
+                        <Legend verticalAlign="top" height={36}/>
+                        <Bar dataKey="confidence" fill="#007bff" onClick={(topic, index) => this.showTopic(topic)} />
                     </BarChart>
                 </ResponsiveContainer>
-                { this.state.showResults ? 
-                    this.state.currentTopic.words.split(", ").map(word => (
-                        <span key={word}>
-                            <Link to={`/search/${word}`} key={word}>
-                                <span className="word"> {word}</span>
-                            </Link>
-                        </span>
-                    )).reduce((previous, current) => [previous, ', ', current])
-                
-                : null }
-
+                <div className="words">
+                    { this.state.showResults ? 
+                        this.state.currentTopic.words.split(" ").map(word => (
+                            <span key={word}>
+                                <Link to={`/search/${word}`} key={word}>
+                                    <span className="word"> {word}</span>
+                                </Link>
+                            </span>
+                        ))
+                    
+                    : null }
+                </div>
             </div>
         );
     }
