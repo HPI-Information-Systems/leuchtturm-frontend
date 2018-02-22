e48aea6cb3aa94db29b47d45ec4ef38759c3e7bb
Nico Scordialo <nico.scordialo@student.hpi.uni-potsdam.de>
2018-02-22 14:34:21 +0100

    Satisfy Mr. Linter. Ref meta#182.

--- client/lampenhaus/src/components/CorrespondentView/TopicList/TopicList.js
+++ client/lampenhaus/src/components/CorrespondentView/TopicList/TopicList.js
@@ -1,16 +1,14 @@
-/* eslint-disable */
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
-import { BarChart, ResponsiveContainer, Bar, Legend, Tooltip, YAxis, XAxis  } from 'recharts';
-import { ListGroup, ListGroupItem, Badge } from 'reactstrap';
+import { BarChart, ResponsiveContainer, Bar, Legend, Tooltip, YAxis, XAxis } from 'recharts';
 import PropTypes from 'prop-types';
 import './TopicList.css';
 import Spinner from '../../Spinner/Spinner';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class TopicList extends Component {
-    constructor(props){
-        super(props)
+    constructor(props) {
+        super(props);
         this.state = {
             showResults: false,
             currentTopic: null,
@@ -20,49 +18,54 @@ class TopicList extends Component {
 
     showTopic(topic) {
         this.setState({ showResults: true });
-        this.state.currentTopic = topic
+        this.state.currentTopic = topic;
     }
 
     render() {
-        var label_topics = this.props.topics.map((topic) => {
-            return {
+        const labelTopics = this.props.topics.map(topic => ({
             confidence: topic.confidence,
-            words: topic.words.map((word) => {return word.word}).join(" ")
-        }})
-          
+            words: topic.words.map(word => word.word).join(' '),
+        }));
 
         return (
-            <div>
-                <ResponsiveContainer width={"100%"} height={200}>
-                    <BarChart data={
-                        label_topics
-                    }>
-                        <XAxis dataKey="words"  tick={false}/>
-                        <YAxis/>
-                        <Tooltip />
-                        <Legend verticalAlign="top" height={36}/>
-                        <Bar dataKey="confidence" fill="#007bff" onClick={(topic, index) => this.showTopic(topic)} />
-                    </BarChart>
-                </ResponsiveContainer>
-                <div className="words">
-                    { this.state.showResults ? 
-                        this.state.currentTopic.words.split(" ").map(word => (
-                            <span key={word}>
-                                <Link to={`/search/${word}`} key={word}>
-                                    <span className="word"> {word}</span>
-                                </Link>
-                            </span>
-                        ))
-                    
-                    : null }
+            this.props.isFetching
+                ? (<Spinner />) :
+                <div>
+                    <ResponsiveContainer width="100%" height={200}>
+                        <BarChart data={
+                            labelTopics
+                        }
+                        >
+                            <XAxis dataKey="words" tick={false} />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend verticalAlign="top" height={36} />
+                            <Bar
+                                dataKey="confidence"
+                                fill="#007bff"
+                                onClick={
+                                    topic => this.showTopic(topic)
+                                }
+                            />
+                        </BarChart>
+                    </ResponsiveContainer>
+                    <div className="words">
+                        { this.state.showResults ?
+                            this.state.currentTopic.words.split(' ').map(word => (
+                                <span key={word}>
+                                    <Link to={`/search/${word}`} key={word}>
+                                        <span className="word"> {word}</span>
+                                    </Link>
+                                </span>
+                            ))
+                            : null }
+                    </div>
                 </div>
-            </div>
         );
     }
 }
 
 TopicList.propTypes = {
-    emailAddress: PropTypes.string.isRequired,
     topics: PropTypes.arrayOf(PropTypes.shape({
         confidence: PropTypes.number.isRequired,
         words: PropTypes.arrayOf(PropTypes.shape({
