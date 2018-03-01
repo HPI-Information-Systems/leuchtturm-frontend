76ff2a68ae15c699f97858fcf8ad0cb2a2f4539f
Dennis Kipping <dennis.kipping@student.hpi.uni-potsdam.de>
2018-03-01 18:36:43 +0100

    all directions and relations present now

--- common/neo4j_requester.py
+++ common/neo4j_requester.py
@@ -35,9 +35,10 @@ class Neo4jRequester:
     def get_graph_for_email_address(self, mail):
         """Return graph for a given email address."""
         graph = {"nodes": [], "links": []}
+        addresses = [mail]
         with self.driver.session() as session:
             with session.begin_transaction() as tx:
-                for sender in tx.run("MATCH(sender:Person{email: $sender_mail}) RETURN toInteger(id(sender)) AS id",
+                for sender in tx.run("MATCH(sender:Person{email: $sender_mail}) RETURN id(sender) AS id",
                                      sender_mail=mail):
                     graph["nodes"].append({
                         "id": sender["id"],
@@ -49,27 +50,34 @@ class Neo4jRequester:
                             "__color": '#000000'
                         }
                     })
-                    for relation in tx.run("MATCH(:Person {email: $sender_mail})-[w:WRITESTO]-(correspondent:Person) "
-                                           "RETURN id(correspondent), correspondent.email, id(w), w.mail_list "
-                                           "ORDER BY size(w.mail_list) DESC LIMIT 10",
-                                           sender_mail=mail):
-                        graph["nodes"].append({
-                            "id": relation["id(correspondent)"],
-                            "type": 'person',
-                            "icon": '\uf2be',
-                            "props": {
-                                "name": relation["correspondent.email"],
-                                "__radius": 16,
-                                "__color": '#000000'
-                            }
-                        })
+                    for node in tx.run("MATCH(:Person {email: $sender_mail})-[w:WRITESTO]-(correspondent:Person) "
+                                            "RETURN id(correspondent), correspondent.email, w.mail_list "
+                                            "ORDER BY size(w.mail_list) DESC LIMIT 10",
+                                            sender_mail=mail):
+                        if not node["correspondent.email"] in addresses:
+                            addresses.append(node["correspondent.email"])
+                            graph["nodes"].append({
+                                "id": node["id(correspondent)"],
+                                "type": 'person',
+                                "icon": '\uf2be',
+                                "props": {
+                                    "name": node["correspondent.email"],
+                                    "__radius": 16,
+                                    "__color": '#000000'
+                                }
+                            })
+
+                    for relation in tx.run("MATCH(personA:Person)-[w:WRITESTO]->(personB:Person) "
+                                            "WHERE personA.email IN $addresses AND personB.email IN $addresses "
+                                            "RETURN id(personA), id(personB), id(w)",
+                                            addresses=addresses):
                         graph["links"].append({
                             "id": relation["id(w)"],
                             "type": '',
                             "props": {},
-                            "source": sender["id"],
-                            "target": relation["id(correspondent)"],
-                            "sourceId": sender["id"],
-                            "targetId": relation["id(correspondent)"]
+                            "source": relation["id(personA)"],
+                            "target": relation["id(personB)"],
+                            "sourceId": relation["id(personA)"],
+                            "targetId": relation["id(personB)"]
                         })
         return graph
