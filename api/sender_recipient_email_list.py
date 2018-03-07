6ad6c651cfee713d683834e47055fa15a97f6c12
Jonas Hering <jonas.hering@student.hpi.de>
2018-03-07 18:17:19 +0100

    Add advanced email list api

--- api/sender_recipient_email_list.py
+++ api/sender_recipient_email_list.py
@@ -3,7 +3,7 @@ from flask import request
 from common.query_builder import QueryBuilder
 from common.util import json_response_decorator, parse_solr_result, parse_email_list, get_default_core
 
-DEFAULT_LIMIT = 10000
+DEFAULT_LIMIT = 100
 DEFAULT_OFFSET = 0
 
 
@@ -16,15 +16,20 @@ class SenderRecipientEmailList:
 
     @json_response_decorator
     def get_sender_recipient_email_list():
-        core = request.args.get('core', default=get_default_core(), type=str)
-        sender = request.args.get('sender')
-        recipient = request.args.get('recipient')
+        core = request.args.get('core', type=str, default=get_default_core())
+        sender = request.args.get('sender', type=str, default='*')
+        recipient = request.args.get('recipient', type=str, default='*')
+        sender_or_recipient = request.args.get('sender_or_recipient', type=str)
         limit = request.args.get('limit', type=int, default=DEFAULT_LIMIT)
         offset = request.args.get('offset', type=int, default=DEFAULT_OFFSET)
-        if not sender or not recipient:
-            raise SyntaxError('Please provide a sender and a recipient')
+        if sender == '*' and recipient == '*' and not sender_or_recipient:
+            raise SyntaxError('Please provide sender or recipient or both or sender_or_recipient.')
 
-        query = 'header.sender.email:' + sender + ' AND header.recipients:*' + recipient + '*'
+        query = ''
+        if sender_or_recipient:
+            query = 'header.sender.email:{0} OR header.recipients:*{0}*'.format(sender_or_recipient)
+        else:
+            query = 'header.sender.email:{0} AND header.recipients:*{1}*'.format(sender, recipient)
 
         query_builder = QueryBuilder(
             core,
