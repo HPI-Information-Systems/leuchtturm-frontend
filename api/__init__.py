ab2b80c901baa5a947637915f5673a1052b29ae9
Dennis Kipping <dennis.kipping@student.hpi.uni-potsdam.de>
2018-03-07 14:22:31 +0100

    snapshot

--- api/__init__.py
+++ api/__init__.py
@@ -2,3 +2,16 @@
 
 Logic for e.g. search requests, retrieval of files etc. goes here.
 """
+
+import configparser
+from pathlib import Path
+from os import environ as env
+
+if 'DATASET' not in env:
+    dataset = 'enron'
+else:
+    dataset = env['DATASET']
+
+config_path = Path('.').parent / 'config' / 'config-' + dataset + '.ini'
+config = configparser.ConfigParser()
+config.read(config_path)
