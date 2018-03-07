9dcac3c548f31eb76a93fdac6135e7e4de91af27
Alexander Zenner <alexander.zenner@student.hpi.uni-potsdam.de>
2018-03-07 18:09:49 +0100

    satisfy linter

--- api/api_blueprint.py
+++ api/api_blueprint.py
@@ -38,6 +38,7 @@ def terms():
 def correspondents_for_term():
     return Terms.get_correspondents_for_term()
 
+
 @api_blueprint.route('/dates_for_term', methods=['GET'])
 def dates_for_term():
     return Terms.get_dates_for_term()
