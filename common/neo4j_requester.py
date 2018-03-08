448f86caf85b300888af08fd560f12c8c09a2387
Alexander Zenner <alexander.zenner@student.hpi.uni-potsdam.de>
2018-03-08 14:25:58 +0100

    refactor neo4j request so that we can request the correspondents for each direction or both directions for a given email address

--- common/neo4j_requester.py
+++ common/neo4j_requester.py
@@ -13,19 +13,40 @@ class Neo4jRequester:
                             ":",
                             port])
         self.driver = GraphDatabase.driver(self.uri)
-        self.results = []
 
-    def get_correspondents_for_email_address(self, mail):
-        """Search correspondents by mail-address of sender."""
+    def get_all_correspondents_for_email_address(self, email_address):
+        return self.get_correspondents_for_email_address(email_address, "both")
+
+    def get_sending_correspondents_for_email_address(self, email_address):
+        """Get correspondents that send emails to a given email_address"""
+        return self.get_correspondents_for_email_address(email_address, "from")
+
+    def get_receiving_correspondents_for_email_address(self, email_address):
+        """Get correspondents that send emails to a given email_address"""
+        return self.get_correspondents_for_email_address(email_address, "to")
+
+    def get_correspondents_for_email_address(self, email_address, direction="both"):
+        """Fetch correspondents from neo4j for given email_address and communication direction"""
+        neo4j_direction = "-[w:WRITESTO]-"
+        if direction == "from":
+            neo4j_direction = "<-[w:WRITESTO]-"
+        elif direction == "to":
+            neo4j_direction = "-[w:WRITESTO]->"
+
+        results = []
+
         with self.driver.session() as session:
             with session.begin_transaction() as tx:
-                for record in tx.run("MATCH (:Person {email: $sender_mail})-[w:WRITESTO]-(correspondent:Person) "
-                                     "RETURN correspondent.email, size(w.mail_list)",
-                                     sender_mail=mail):
+                for record in tx.run("MATCH (:Person {email: $email_address})" +
+                                     neo4j_direction +
+                                     "(correspondent:Person) "
+                                     "RETURN correspondent.email, size(w.mail_list) "
+                                     "ORDER BY size(w.mail_list) DESC",
+                                     email_address=email_address):
                     correspondent = dict(email_address=record["correspondent.email"],
                                          count=record["size(w.mail_list)"])
-                    self.results.append(correspondent)
-        return self.results
+                    results.append(correspondent)
+        return results
 
     def get_graph_for_email_address(self, mail):
         """Return graph for a given email address."""
