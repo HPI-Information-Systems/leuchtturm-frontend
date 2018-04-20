e40232ee1bbb24026c1d685dbc69e37e932dad6c
Jakob Edding <jakob.edding@student.hpi.uni-potsdam.de>
2018-04-20 12:04:13 +0200

    Fix how local and global updates of search parameters are handled meta#331

--- client/lampenhaus/src/components/SearchBar/SearchBar.js
+++ client/lampenhaus/src/components/SearchBar/SearchBar.js
@@ -7,52 +7,81 @@ class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            newSearchTerm: '',
+            tempSearchTerm: '',
+            tempStartDate: '',
+            tempEndDate: '',
         };
-        this.onUpdateSearchTerm = this.onUpdateSearchTerm.bind(this);
+        this.commitSearch = this.commitSearch.bind(this);
     }
 
     componentWillReceiveProps(nextProps) {
-        this.state.newSearchTerm = nextProps.searchTerm;
+        if (this.relevantPropsChanged(nextProps)) {
+            this.state.tempSearchTerm = nextProps.searchTerm;
+            this.state.tempStartDate = nextProps.startDate;
+            this.state.tempEndDate = nextProps.endDate;
+        }
     }
 
-    onUpdateSearchTerm(e) {
-        this.setState({ newSearchTerm: e.target.value });
+    onUpdateSearchTerm(tempSearchTerm) {
+        this.setState({ tempSearchTerm });
+    }
+
+    onUpdateStartDate(tempStartDate) {
+        this.setState({ tempStartDate });
+    }
+
+    onUpdateEndDate(tempEndDate) {
+        this.setState({ tempEndDate });
+    }
+
+    relevantPropsChanged(nextProps) {
+        return (
+            this.props.searchTerm !== nextProps.searchTerm ||
+            this.props.startDate !== nextProps.startDate ||
+            this.props.endDate !== nextProps.endDate
+        );
     }
 
     handleKeyPress(e) {
         if (e.key === 'Enter') {
-            this.props.updateBrowserSearchPath(this.state.newSearchTerm);
+            this.commitSearch();
         }
+    }
+
+    commitSearch() {
+        this.props.changeStartDateHandler(this.state.tempStartDate);
+        this.props.changeEndDateHandler(this.state.tempEndDate);
+        this.props.updateBrowserSearchPath(this.state.tempSearchTerm);
     }
 
     render() {
         return (
-            <InputGroup>
+            <InputGroup
+                onKeyPress={e => this.handleKeyPress(e)}
+            >
                 <Input
-                    onKeyPress={e => this.handleKeyPress(e)}
                     placeholder="Enter search term"
-                    value={this.state.newSearchTerm}
-                    onChange={this.onUpdateSearchTerm}
+                    value={this.state.tempSearchTerm}
+                    onChange={e => this.onUpdateSearchTerm(e.target.value)}
                 />
                 <InputGroupAddon addonType="append">
                     <InputGroupText>From:</InputGroupText>
                     <Input
                         type="date"
-                        value={this.props.startDate}
-                        onChange={e => this.props.changeStartDateHandler(e.target.value)}
                         className="input-in-group-addon"
+                        value={this.state.tempStartDate}
+                        onChange={e => this.onUpdateStartDate(e.target.value)}
                     />
                     <InputGroupText>To:</InputGroupText>
                     <Input
                         type="date"
-                        value={this.props.endDate}
-                        onChange={e => this.props.changeEndDateHandler(e.target.value)}
                         className="input-in-group-addon"
+                        value={this.state.tempEndDate}
+                        onChange={e => this.onUpdateEndDate(e.target.value)}
                     />
                     <Button
                         color="primary"
-                        onClick={() => this.props.updateBrowserSearchPath(this.state.newSearchTerm)}
+                        onClick={this.commitSearch}
                     >
                         Search
                     </Button>
