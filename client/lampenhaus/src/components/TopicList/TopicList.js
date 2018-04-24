dce57757e96599d8543615d8b4639ed6843ad337
Nico Scordialo <nico.scordialo@student.hpi.uni-potsdam.de>
2018-04-24 17:29:16 +0200

    Add hard coded data and baseline mechanics for Topic Space.

--- client/lampenhaus/src/components/TopicList/TopicList.js
+++ client/lampenhaus/src/components/TopicList/TopicList.js
@@ -25,6 +25,87 @@ class TopicList extends Component {
         this.setState({ currentTopic: topic });
     }
 
+    componentDidUpdate(){
+        d3.select("svg").html("");
+        var svg = d3.select("svg")
+            
+
+        var nodes_data =  [
+            {"topic": "a", "fx":250, "fy":0, "id":1},
+            {"topic": "b", "fx":0, "fy":250, "id":2},
+            {"topic": "c", "fx":250, "fy":500, "id":3},
+            {"topic": "d", "fx":500, "fy":250, "id":4},
+            {"person": "e", "id":5},
+            {"person": "f", "id":6},
+        ]
+
+        var links_data = [
+            {"source": 1, "target": 5},
+            {"source": 2, "target": 5},
+            {"source": 3, "target": 5},
+            {"source": 4, "target": 5},
+            {"source": 1, "target": 6},
+            {"source": 2, "target": 6},
+            {"source": 3, "target": 6},
+            {"source": 4, "target": 6},
+            ]
+        //Create the link force 
+        //We need the id accessor to use named sources and targets 
+        var link_force =  d3.forceLink(links_data).strength(Math.random)
+            .id(function(d) { return d.id; })
+        
+        //set up the simulation 
+        //nodes only for now 
+        var simulation = d3.forceSimulation()
+        .nodes(nodes_data);
+
+    
+
+        //draw lines for the links 
+        var link = svg.append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(links_data)
+        .enter().append("line")
+        .attr("stroke-width", 0);    
+
+        simulation.force("links",link_force).force("charge", d3.forceManyBody())
+
+        function circleColour(d){
+            if(d.person){
+                return "blue";
+            } else {
+                return "white";
+            }
+        }
+        
+        //draw circles for the nodes 
+        var node = svg.append("g")
+                .attr("class", "nodes")
+                .selectAll(".dennis")
+                .data(nodes_data)
+                .enter()
+                .append("circle")
+                .attr("r", 20)
+                .attr("fill", circleColour);
+
+
+        function tickActions() {
+            //update circle positions to reflect node updates on each tick of the simulation 
+            node
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; })
+
+            link
+                .attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+          }
+        
+        simulation.on("tick", tickActions );
+    }
+
     render() {
         const stringTopics = this.props.topics.map(topic => ({
             confidence: topic.confidence,
