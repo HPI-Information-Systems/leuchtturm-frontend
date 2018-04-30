35d7631d159ea81038a69d20e3a86c54b30ea1ad
Jakob Edding <jakob.edding@student.hpi.uni-potsdam.de>
2018-04-30 16:55:11 +0200

    Adapt topic parsing for correspondent to new Solr collection for topics meta#368

--- api/topics.py
+++ api/topics.py
@@ -6,9 +6,11 @@ from common.query_builder import QueryBuilder
 import json
 from ast import literal_eval as make_tuple
 from common.util import json_response_decorator, build_time_filter
+from ast import literal_eval
 
 SOLR_MAX_INT = 2147483647
 LIMIT = 100
+FACET_LIMIT = 1000
 
 
 class Topics(Controller):
@@ -20,6 +22,75 @@ class Topics(Controller):
 
     @json_response_decorator
     def get_topics_for_correspondent():
+        dataset = Controller.get_arg('dataset')
+        email_address = Controller.get_arg('email_address')
+        date_range_filter_query = build_time_filter(
+            Controller.get_arg('start_date', required=False), Controller.get_arg('end_date', required=False))
+
+        join_query = '{!join from=doc_id fromIndex=' + dataset + ' to=doc_id}header.sender.email:' + email_address + \
+                     '&fq={!join from=doc_id fromIndex=' + dataset + ' to=doc_id}' + date_range_filter_query
+
+        facet_query = {
+            'facet_topic_id': {
+                'type': 'terms',
+                'field': 'topic_id',
+                'facet': {
+                    'sum_of_confs_for_topic': 'sum(topic_conf)',
+                    'facet_terms': {
+                        'type': 'terms',
+                        'field': 'terms'
+                    }
+                },
+                'sort': 'index asc',
+                'limit': FACET_LIMIT,
+                'refine': True
+            }
+        }
+        query = '*:*' + '&json.facet=' + json.dumps(facet_query)
+
+        query_builder_topic_distribution = QueryBuilder(
+            dataset=dataset,
+            query=query,
+            fq=join_query,
+            limit=0,
+            core_type='Core-Topics'
+        )
+        solr_result_topic_distribution = query_builder_topic_distribution.send()
+
+        query_builder_doc_count_for_correspondent = QueryBuilder(
+            dataset=dataset,
+            query='header.sender.email:' + email_address,
+            fq=date_range_filter_query,
+            limit=0
+        )
+        solr_result_email_count = query_builder_doc_count_for_correspondent.send()
+        total_email_count = solr_result_email_count['response']['numFound']
+
+        parsed_topics = list(map(
+            Topics.parse_topic_closure_wrapper(total_email_count),
+            solr_result_topic_distribution['facets']['facet_topic_id']['buckets']
+        ))
+
+        return parsed_topics
+
+    @staticmethod
+    def parse_topic_closure_wrapper(total_email_count):
+        def parse_topic(raw_topic):
+            parsed_topic = dict()
+            parsed_topic['topic_id'] = raw_topic['val']
+            parsed_topic['confidence'] = raw_topic['sum_of_confs_for_topic'] / total_email_count
+            word_confidence_tuples_serialized = raw_topic['facet_terms']['buckets'][0]['val'] \
+                .replace('(', '\"(').replace(')', ')\"')
+            word_confidence_tuples = [literal_eval(tuple) for tuple in json.loads(word_confidence_tuples_serialized)]
+            parsed_topic['words'] = [
+                {'word': tuple[0], 'confidence': tuple[1]}
+                for tuple in word_confidence_tuples
+            ]
+            return parsed_topic
+        return parse_topic
+
+    @json_response_decorator
+    def get_topics_for_correspondent_old():
         dataset = Controller.get_arg('dataset')
         email_address = Controller.get_arg('email_address')
         filter_query = build_time_filter(Controller.get_arg('start_date',
@@ -36,7 +107,8 @@ class Topics(Controller):
 
         solr_result = query_builder.send()
 
-        return Topics.build_topic_result(solr_result)
+        asdf = Topics.build_topic_result(solr_result)
+        return asdf
 
     @staticmethod
     def build_topic_result(solr_result):
