a854c69ba05ec09679cd65fea3510ce2f48aa59c
Jakob Edding <jakob.edding@student.hpi.uni-potsdam.de>
2018-05-02 15:01:37 +0200

    Remove obsolete code and dependencies meta#368

--- api/topics.py
+++ api/topics.py
@@ -1,12 +1,10 @@
 """The topics api route can be used to get topics for a mail address from solr."""
 
 from api.controller import Controller
-import pandas as pd
 from common.query_builder import QueryBuilder
 import json
 from ast import literal_eval as make_tuple
 from common.util import json_response_decorator, build_time_filter
-from ast import literal_eval
 
 SOLR_MAX_INT = 2147483647
 LIMIT = 100
@@ -88,93 +86,10 @@ class Topics(Controller):
             parsed_topic['confidence'] = raw_topic['sum_of_confs_for_topic'] / total_email_count
             word_confidence_tuples_serialized = raw_topic['facet_terms']['buckets'][0]['val'] \
                 .replace('(', '\"(').replace(')', ')\"')
-            word_confidence_tuples = [literal_eval(tuple) for tuple in json.loads(word_confidence_tuples_serialized)]
+            word_confidence_tuples = [make_tuple(tuple) for tuple in json.loads(word_confidence_tuples_serialized)]
             parsed_topic['words'] = [
                 {'word': tuple[0], 'confidence': tuple[1]}
                 for tuple in word_confidence_tuples
             ]
             return parsed_topic
         return parse_topic
-
-    @json_response_decorator
-    def get_topics_for_correspondent_old():
-        dataset = Controller.get_arg('dataset')
-        email_address = Controller.get_arg('email_address')
-        filter_query = build_time_filter(Controller.get_arg('start_date',
-                                                            required=False),
-                                         Controller.get_arg('end_date', required=False))
-
-        query = 'header.sender.email:' + email_address
-        query_builder = QueryBuilder(
-            dataset=dataset,
-            query=query,
-            fq=filter_query,
-            limit=LIMIT
-        )
-
-        solr_result = query_builder.send()
-
-        asdf = Topics.build_topic_result(solr_result)
-        return asdf
-
-    @staticmethod
-    def build_topic_result(solr_result):
-        # a list of topic distributions for each mail, each topic as a string
-        topic_distributions_per_mail_s = list(map(lambda topic_distribution_s:
-                                                  json.loads(topic_distribution_s["topics"][0]),
-                                                  solr_result['response']['docs']))
-
-        num_mails = len(topic_distributions_per_mail_s)
-
-        # no topics found
-        if not topic_distributions_per_mail_s:
-            return []
-
-        actual_t_dists_per_mail = []
-
-        # extract the actual topic distributions for each mail in the correct format [topic_confidence,
-        # [[word, word_confidence]...]...]
-        for t_dist_s in topic_distributions_per_mail_s:
-            actual_dist = list(map(lambda topic_distribution_l_of_s: make_tuple(topic_distribution_l_of_s), t_dist_s))
-            actual_t_dists_per_mail.append(actual_dist)
-
-        # introduce rest topics for each mail
-        for t_dist in actual_t_dists_per_mail:
-            sum_confs = sum(float(topic[0]) for topic in t_dist)
-            t_dist.append((1 - sum_confs, []))
-
-        # flatten the resulting list of lists
-        flattened_topics_over_all_mails = [item for sublist in actual_t_dists_per_mail for item in sublist]
-
-        # use Pandas dataframe for the aggregation of confidence
-        df = pd.DataFrame(flattened_topics_over_all_mails)
-
-        # convert string confidences to float
-        df[0] = df[0].astype(float)
-
-        # convert topic distribution list to tuple so that it can be aggregated
-        df[1] = df[1].apply(tuple)
-
-        # perform aggregation for average topic confidence
-        topics_with_sum_conf = df.groupby([1], as_index=False).sum()
-
-        topics_with_sum_conf[0] = topics_with_sum_conf[0].divide(num_mails)
-
-        topics_with_avg_conf = topics_with_sum_conf
-
-        # retransform tuples to lists
-        topics_with_avg_conf[1] = topics_with_avg_conf[1].apply(list)
-
-        # df to list of tuples
-        topics_with_conf_l = [tuple(x) for x in topics_with_avg_conf.values]
-
-        # parse every tuple into a more easily accessible object
-        topics_as_objects = list(map(lambda topic_tuple: {"confidence": round(float(topic_tuple[1]), 4),
-                                                          "words": topic_tuple[0]}, topics_with_conf_l))
-
-        # parse every word entry for each topic in the same way as above and sort them by confidence
-        for topic in topics_as_objects:
-            topic["words"] = list(map(lambda word: {"word": word[0], "confidence": float(word[1])}, topic["words"]))
-            topic["words"] = sorted(topic["words"], key=lambda word: word['confidence'], reverse=True)
-
-        return topics_as_objects
