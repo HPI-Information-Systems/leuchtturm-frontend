6e57273f0a9c62a236afe63cede64e188502b153
Nico Scordialo <nico.scordialo@student.hpi.uni-potsdam.de>
2018-05-08 17:04:48 +0200

    Add method to parse all topics.

--- api/topics.py
+++ api/topics.py
@@ -104,3 +104,22 @@ class Topics(Controller):
             ]
             return parsed_topic
         return parse_topic
+
+    @staticmethod
+    def parse_all_topics(all_topics):
+        def parse_topic(topic):
+            parsed_topic = dict()
+            parsed_topic['topic_id'] = topic['topic_id']
+            parsed_topic['confidence'] = 0
+            word_confidence_tuples_serialized = topic['terms'] \
+                .replace('(', '\"(').replace(')', ')\"')
+            word_confidence_tuples = [make_tuple(tuple) for tuple in json.loads(word_confidence_tuples_serialized)]
+            parsed_topic['words'] = [
+                {'word': tuple[0], 'confidence': tuple[1]}
+                for tuple in word_confidence_tuples
+            ]
+            return parsed_topic
+
+        parsed_topics = [parse_topic(topic) for topic in all_topics]
+
+        return parsed_topics
