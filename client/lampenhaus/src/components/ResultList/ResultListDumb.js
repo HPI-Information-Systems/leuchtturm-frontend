dd9f4e7d9aeb2285c1e3fb50cac1048ef9455cdf
Nico Scordialo <nico.scordialo@student.hpi.uni-potsdam.de>
2018-05-16 18:36:49 +0200

    Modify Result list for topics.

--- client/lampenhaus/src/components/ResultList/ResultListDumb.js
+++ client/lampenhaus/src/components/ResultList/ResultListDumb.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
+import * as d3 from 'd3';
 import PropTypes from 'prop-types';
 import Result from './Result/Result';
 import Spinner from '../Spinner/Spinner';
@@ -8,7 +9,17 @@ import Spinner from '../Spinner/Spinner';
 class ResultListDumb extends Component {
     render() {
         const resultElements = this.props.results.map(result => (
-            <ListGroupItem key={result.doc_id}>
+            <ListGroupItem
+                key={result.doc_id}
+                onMouseEnter={function () {
+                    console.log('MMA');
+                    d3.select(`circle[data-highlight='${result.doc_id}']`).attr('r', '5').attr('fill', 'red');
+                }}
+                onMouseLeave={function () {
+                    console.log('MMA');
+                    d3.select(`circle[data-highlight='${result.doc_id}']`).attr('r', '3').attr('fill', 'black');
+                }}
+            >
                 <Result
                     body={result.body}
                     subject={result.header.subject}
