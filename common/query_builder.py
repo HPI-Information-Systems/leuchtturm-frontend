630ed181ff7ba3cfcd416e2fe4981693b8894564
hendrik <hendrik.schmidt@student.hpi.uni-potsdam.de>
2018-05-17 17:00:32 +0200

    fix wrong queries

--- common/query_builder.py
+++ common/query_builder.py
@@ -164,7 +164,7 @@ def build_fuzzy_solr_query(phrase):
     return query
 
 
-def build_filter_query(filter_object, filter_correspondents=True, is_topic_request=True, join_string=''):
+def build_filter_query(filter_object, filter_correspondents=True, is_topic_request=False, join_string=''):
     filter_query_list = []
 
     if filter_object.get('startDate') or filter_object.get('endDate'):
@@ -191,7 +191,7 @@ def build_filter_query(filter_object, filter_correspondents=True, is_topic_reque
 
     if filter_object.get('selectedTopics'):
         topic_filter_pre = '&fq=' if filter_query or is_topic_request else ''
-        topic_filter_pre += '{!join from=doc_id fromIndex=enron_topics to=doc_id}' if is_topic_request else ''
+        topic_filter_pre += '{!join from=doc_id fromIndex=enron_topics to=doc_id}' if not is_topic_request else ''
         topic_filter = topic_filter_pre + '(topic_id:' \
                        + ' OR topic_id:'.join(str(topic_id) for topic_id in filter_object['selectedTopics']) \
                        + ') AND topic_conf: [' + str(filter_object.get('topicThreshold', '0.2')) + ' TO *]'
