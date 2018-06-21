a8a656d379a434d4ff7c2326cb0174f83c3c286a
Jakob Edding <jakob.edding@student.hpi.uni-potsdam.de>
2018-06-21 19:25:36 +0200

    Fix sizing of category chart to fit card body

--- client/lampenhaus/src/components/CorrespondentView/CategoryChart/CategoryChart.js
+++ client/lampenhaus/src/components/CorrespondentView/CategoryChart/CategoryChart.js
@@ -22,7 +22,7 @@ class CategoryChart extends Component {
             categoryElements = 'No categories with selected filters';
         } else {
             categoryElements = (
-                <ResponsiveContainer width="100%" height={150}>
+                <ResponsiveContainer width="100%" height="100%">
                     <RadarChart
                         data={this.props.categories}
                     >
